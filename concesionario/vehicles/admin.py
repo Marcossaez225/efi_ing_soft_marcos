95c1769776c2316bfc525303b1dabbc81a983f07
Didier Oliva <di.oliva@itecriocuarto.org.ar>
2024-08-11 12:21:21 -0300

    Update vehicles app

--- concesionario/vehicles/admin.py
+++ concesionario/vehicles/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Brand, Country, Vehicle
 
-# Register your models here.
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ('get_brand_name', 'get_country_name', 'model', 'engine_displacement', 'fuel_type', 'number_of_doors', 'year_of_manufacture', 'price_in_usd')
+
+    def get_brand_name(self, obj):
+        return obj.brand.name
+    get_brand_name.short_description = 'Marca'
+
+    def get_country_name(self, obj):
+        return obj.country_of_manufacture.name
+    get_country_name.short_description = 'País de Fabricación'
--- concesionario/vehicles/models.py
+++ concesionario/vehicles/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Country(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Vehicle(models.Model):
+    FUEL_CHOICES = [
+        ('electric', 'Electric'),
+        ('diesel', 'Diesel'),
+        ('hybrid', 'Hybrid'),
+        ('gasoline', 'Gasoline'),
+    ]
+
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name="vehicles")
+    model = models.CharField(max_length=100)
+    year_of_manufacture = models.IntegerField()
+    number_of_doors = models.IntegerField()
+    engine_displacement = models.DecimalField(max_digits=5, decimal_places=2)
+    fuel_type = models.CharField(max_length=10, choices=FUEL_CHOICES)
+    country_of_manufacture = models.ForeignKey(Country, on_delete=models.CASCADE, related_name="manufactured_vehicles")
+    price_in_usd = models.DecimalField(max_digits=12, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.brand.name} {self.model} ({self.year_of_manufacture})"
